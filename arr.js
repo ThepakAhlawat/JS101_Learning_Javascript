31863bfb05eb26e889f449388376297ecb523c4a
DpkAhlawat <25595deepak.2020@gmail.com>
2023-10-24 16:51:04 +0530

    array

---
+++ arr.js
@@ -0,0 +1,10 @@
+let arr=[1,2,3,4,5];
+let position=""
+let index=0
+for(i=0;i<arr.length;i++){
+ 
+    index+=1
+    position=index +" ---⏭️  ⏭️  ⏭️ -----≫  "+arr[i];
+  
+  console.log( position)
+}
